bf28e07a36c43c3bef51b2a42ed7b2d2ca49f0a9
Goizz <gijstempel@msn.com>
2015-01-20 13:57:06 +0100

    Added Sound to splashMenu

--- Assets/Scripts/SplashMenu.cs
+++ Assets/Scripts/SplashMenu.cs
@@ -3,17 +3,14 @@ using System.Collections;
 
 public class SplashMenu : MonoBehaviour {
 	Color color = new Color(1,1,1,0);
-	public float fadeInTime = 2.0f;
-	public float fadeOutTime = 5.0f;
+	public float fadeInTime = 3.3f;
+	public float fadeOutTime = 3.3f;
 	float fadeInTimer = 0.0f;
 	float fadeOutTimer = 0.0f;
 	public Texture habitat1 = null;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+
+
 	// Update is called once per frame
 	void Update () 
 	{
