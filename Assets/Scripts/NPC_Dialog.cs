925b05cfe41ded6eea3e90ed26e260e19424e8aa
Derojo <bakker.rowin@gmail.com>
2015-01-21 14:49:40 +0100

    Changed some settings and set loadimage as sprite

--- Assets/Scripts/NPC_Dialog.cs
+++ Assets/Scripts/NPC_Dialog.cs
@@ -7,7 +7,13 @@ public class NPC_Dialog : MonoBehaviour {
 	public string[] CallButtons;
 	public string[] QuestStates;
 	public GUISkin habitatSkin;
-	
+	public GameObject questmark;
+
+	void Awake() {
+		if (Library.habitat.questData.activeQuest) {
+			questmark.SetActive(false);
+		}
+	}
 	// Use this for initialization
 	void Start () {
 
@@ -32,9 +38,10 @@ public class NPC_Dialog : MonoBehaviour {
 				// Draw acceptbutton
 				if( GUI.Button (new Rect (Screen.width / 3, Screen.height / 1.6f, Screen.width / 7, Screen.height / 7), CallButtons [0])) {
 					// Activate quest if we click the right button
-					Debug.Log ("Activate Quest");
 					Library.habitat.questData.activeQuest = true;
 					Library.habitat.questData.displayQuestlog = false;
+					// Remove questmark
+					questmark.SetActive(false);
 				}
 				if(GUI.Button (new Rect ((Screen.width /100) * 55, Screen.height / 1.6f, Screen.width / 7, Screen.height / 7), CallButtons [1])) {
 					Library.habitat.questData.displayQuestlog = false;
