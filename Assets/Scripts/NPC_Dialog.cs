a237904c7e166c98a49555b9bd9aef374ddf835d
Derojo <bakker.rowin@gmail.com>
2015-01-21 20:46:20 +0100

    Working on quest chain

--- Assets/Scripts/NPC_Dialog.cs
+++ Assets/Scripts/NPC_Dialog.cs
@@ -6,8 +6,12 @@ public class NPC_Dialog : MonoBehaviour {
 	public string QuestTitle;
 	public string[] CallButtons;
 	public string[] QuestStates;
+	public GameObject[] FlowerParts;
+	public string placeUnder = "";
 	public GUISkin habitatSkin;
 	public GameObject questmark;
+
+	private GameObject _spawnPart;
 
 	void Awake() {
 		if (Library.habitat.questData.activeQuest) {
@@ -42,6 +46,9 @@ public class NPC_Dialog : MonoBehaviour {
 					Library.habitat.questData.displayQuestlog = false;
 					// Remove questmark
 					questmark.SetActive(false);
+					// Add linked parts to the scene
+					spawnFlowerParts ();	
+
 				}
 				if(GUI.Button (new Rect ((Screen.width /100) * 55, Screen.height / 1.6f, Screen.width / 7, Screen.height / 7), CallButtons [1])) {
 					Library.habitat.questData.displayQuestlog = false;
@@ -59,6 +66,18 @@ public class NPC_Dialog : MonoBehaviour {
 
 	void OnTriggerExit() {
 		Library.habitat.questData.displayQuestlog = false;
+	}
+
+	void spawnFlowerParts() {
+		foreach(GameObject Part in this.FlowerParts) {
+			if(Part == null) {
+				continue;
+			}
+			_spawnPart = Instantiate(Part, Part.transform.position, Part.transform.rotation) as GameObject;
+			_spawnPart.name = Part.name;
+			_spawnPart.transform.parent = GameObject.Find(placeUnder).transform;
+			_spawnPart.transform.localPosition = Part.transform.position;
+		}
 	}
 
 }
