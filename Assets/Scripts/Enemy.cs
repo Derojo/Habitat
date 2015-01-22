fe4d9c40888757b16ab1fe1c6b827c1c94107c34
Goizz <gijstempel@msn.com>
2015-01-22 10:12:26 +0100

    Added enemy in map3 TEST NEEDED!!!
    - added enemy script
    - added enemy block
    - added trigger

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+	public int maxDistance;
+	private Transform myTransform;
+	public bool follow;
+	
+	void Awake () 
+	{
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag ("Player");
+		if (go != null) 
+		{
+			target = go.transform;
+		}
+		maxDistance = 2;
+	}
+
+		void OnTriggerEnter (Collider other) 
+		{
+			follow = true;
+			if (follow) 
+			{
+				myTransform.rotation = Quaternion.Slerp (myTransform.rotation, Quaternion.LookRotation (target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+				if (Vector3.Distance (target.position, myTransform.position) > maxDistance) 
+				myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+			}
+		}
+
+		void OnTriggerExit (Collider other)
+		{
+			follow = false;
+		}
+
+}
