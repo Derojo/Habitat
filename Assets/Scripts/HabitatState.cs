6852a1e9ed59a38daf943985d745e4cff343aaa1
Derojo <bakker.rowin@gmail.com>
2015-02-03 11:29:09 +0100

    Changed imagetarget from stones to habitat

--- Assets/Scripts/HabitatState.cs
+++ Assets/Scripts/HabitatState.cs
@@ -22,7 +22,7 @@ public class HabitatState : MonoBehaviour {
 	public static bool allowShowingTargetContent=true;
 	public static bool doNotHideARContent=false;
 	public Status initialStatus = Status.Stopped;
-	private static float targetWidth = (Screen.width/2);
+	private static float targetWidth = (Screen.width/1.8f);
 	private float targetHeight = (targetWidth/100f)*70f;
 	public AudioClip ambientSound;
 	private bool _isPlaying = false;
@@ -69,15 +69,17 @@ public class HabitatState : MonoBehaviour {
 	/// Draw targetfinder and information to get the focus back on the game board
 	private void DrawTargetWindow ()
 	{
+		GUI.skin.GetStyle("labelCenterWithBack").fontSize = Screen.width / 30;
+		GUI.skin.GetStyle("LabelCenterBackground").fontSize = Screen.width / 30;
 		//GUI.Box(new Rect(0, 0, LigaboUtils.screenWidth, LigaboUtils.screenHeight), "", GUI.skin.GetStyle("scoreboard") );
 		GUI.Box(new Rect(Screen.width/2-targetWidth/2,Screen.height/2-targetHeight/2,targetWidth,targetHeight),"",GUI.skin.GetStyle("targetFinder"));
 
-		var boardWidth = targetWidth*0.8f;
-		var boardHeight = targetHeight*0.7f;
+		var boardWidth = targetWidth*0.85f;
+		var boardHeight = targetHeight*0.8f;
 		GUI.DrawTexture(new Rect(Screen.width/2-boardWidth/2, Screen.height/2-boardHeight/2,boardWidth,boardHeight),miniBoardIcon);
 		
-		var labelRect = new Rect(Screen.width/2-boardWidth/2*0.9f, Screen.height/2-40,boardWidth*0.9f,80);
-		GUI.Label(labelRect,"",GUI.skin.GetStyle("labelCenterWithBack"));
+		var labelRect = new Rect(Screen.width/2-boardWidth/2*0.9f, Screen.height/8,boardWidth*0.9f,80);
+		GUI.Label(new Rect(Screen.width/2-boardWidth/2*0.9f, Screen.height/7.9f,boardWidth*0.9f,80),"Richt je camera op het game board", GUI.skin.GetStyle("LabelCenterBackground"));
 		GUI.Label(labelRect,"Richt je camera op het game board", GUI.skin.GetStyle("labelCenterWithBack"));
 
 	}
